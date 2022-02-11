741b0d097957eaa27b41e131e995a1c9ac3610ff
Mateus Quintino <mateusq1@hotmail.com>
2022-02-11 17:36:14 -0300

    Finalizando o CRUD

--- src/main/java/br/com/mquintino/forum/controller/TopicosController.java
+++ src/main/java/br/com/mquintino/forum/controller/TopicosController.java
@@ -2,19 +2,26 @@ package br.com.mquintino.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.mquintino.forum.controller.dto.DetalhesTopicoDto;
 import br.com.mquintino.forum.controller.dto.TopicoDto;
+import br.com.mquintino.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.mquintino.forum.controller.form.TopicoForm;
 import br.com.mquintino.forum.modelo.Topico;
 import br.com.mquintino.forum.repository.CursoRepository;
@@ -30,9 +37,9 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;	
 	
+	// SELECIONAR TODOS OS CAMPOS
 	@GetMapping()
-	public List<TopicoDto> lista(String nomeCurso) {			
-		
+	public List<TopicoDto> lista(String nomeCurso) {					
 		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDto.converter(topicos);
@@ -43,13 +50,55 @@ public class TopicosController {
 		}				
 	}
 	
+	// SELECIONAR POR ID
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesTopicoDto> detalhar (@PathVariable Long id) {
+		//Topico topico = topicoRepository.getById(id);
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	// INCLUIR UM NOVO REGISTRO
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+	@Transactional  // >> isso aqui faz o processo commitar após encerrar o método
+	public ResponseEntity<TopicoDto> cadastrar (@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	// ATUALIZAR UM REGISTRO
+	@PutMapping("/{id}")
+	@Transactional 
+	public ResponseEntity<TopicoDto> atualizar (@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm topicoForm){
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {		
+			Topico topico = topicoForm.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		
+		return ResponseEntity.notFound().build();
+				
+	}
+	
+	// DELETAR UM REGISTRO
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover (@PathVariable Long id){
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {		
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.notFound().build();				
 	}
 	
 	
--- src/main/java/br/com/mquintino/forum/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/br/com/mquintino/forum/controller/form/AtualizacaoTopicoForm.java
@@ -4,43 +4,40 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-import br.com.mquintino.forum.modelo.Curso;
 import br.com.mquintino.forum.modelo.Topico;
-import br.com.mquintino.forum.repository.CursoRepository;
+import br.com.mquintino.forum.repository.TopicoRepository;
 
-public class TopicoForm {
-
+public class AtualizacaoTopicoForm {
+	
 	@NotNull  @Length(min = 4) 
 	private String titulo;
 	
 	@NotNull @Length(min = 10) 
 	private String mensagem;
 	
-	@NotNull
-	private String nomeCurso;
-	
-	public Topico converter(CursoRepository cursoRepository) {
-		Curso curso = cursoRepository.findByNome(nomeCurso);
-		return new Topico(titulo, mensagem, curso);
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		
+		Topico topico = topicoRepository.getById(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
 	}
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getMensagem() {
 		return mensagem;
 	}
+
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
-	public String getNomeCurso() {
-		return nomeCurso;
-	}
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	
+
 }
